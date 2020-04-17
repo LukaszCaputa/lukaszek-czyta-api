25bab2171c8ccc743ea5622e8ff66048555ede0a
Lukasz <lukasz@lukaszcaputa.eu>
2020-04-17 21:09:26 +0200

    removing secrets

--- app/secret.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    host: 'lukaszekczyta-db.cx8pc3ou6ygr.eu-central-1.rds.amazonaws.com',
-    user: 'lukcap',
-    password: '7J64RaHprAqb6lss2Rja',
-    database: 'booksdb'
-  }
