2094599d7798d5a5d82c90521341fe7c261fb2aa
Lukasz <lukasz@lukaszcaputa.eu>
2020-04-20 21:31:17 +0200

    added CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to lukaszek-czyta-api"});
